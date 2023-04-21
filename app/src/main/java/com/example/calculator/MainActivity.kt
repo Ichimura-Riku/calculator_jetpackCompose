8532059cdd967fbee11aac4193da9647bd93f3d8
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-04-21 20:33:11 +0900

    ヘッダーと計算画面のカードを作成 カードの丸みをつけたいけどうまくいってないからそこを何とかしたい

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.preference.PreferenceActivity.Header
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +27,91 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculatorTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(modifier: Modifier = Modifier) {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            Header(modifier = Modifier)
+            OutPutCalcState(modifier = Modifier)
+        }
+    }
+
 }
+
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.primary) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 55.dp, vertical = 15.dp
+                ),
+//            verticalArrangement = Arrangement,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "calculator", style = MaterialTheme.typography.headlineSmall.copy(
+                    fontWeight = FontWeight.Medium
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun OutPutCalcState(modifier: Modifier = Modifier) {
+    Surface(
+        modifier.fillMaxWidth(),
+        color = MaterialTheme.colorScheme.secondary,
+
+
+        ) {
+        Text(
+            text = "1103865",
+            modifier = modifier
+                .padding(vertical = 80.dp, horizontal = 10.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.secondary,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 28.dp, 28.dp)
+                ),
+            style = MaterialTheme.typography.displayMedium.copy(
+                fontWeight = FontWeight.ExtraBold
+            ),
+            textAlign = TextAlign.End
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CalculatorTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        Header()
+        MyApp(modifier = Modifier)
     }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    CalculatorTheme {
+//        Greeting("Android")
+        Header()
+        Header(modifier = Modifier)
+    }
+
 }
